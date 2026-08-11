fd3f8cc7d77614ad4ea120771fe89afaf04c635c
qgeng1465 <gengqiushuo2004@qq.com>
2026-08-11 15:00:40 +0800

    update src/bio_mcp/__init__.py

--- src/bio_mcp/__init__.py
+++ src/bio_mcp/__init__.py
@@ -6,6 +6,6 @@ literature / sequences / BLAST / structures / enrichment / annotations /
 genomes / interactions / variants / domains / compounds / single-cell / etc.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = ["__version__"]
